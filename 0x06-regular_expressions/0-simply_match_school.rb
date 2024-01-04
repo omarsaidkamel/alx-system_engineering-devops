b7ed540f35d3be2e6b5e809b93f4ee73b526d1b2
root <osk09876@gmail.com>
2024-01-04 04:09:43 -0800

    done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env ruby
-# A regular expression that is simply matching School
-puts ARGV[0].scan(/School/m).join
+
+input = ARGV[0]
+
+if input =~ /School/
+  puts "School"
+else
+  puts ""
+end
