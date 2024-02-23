01071d548e6dbfb379144d6e02e4668ddfce17d0
omarsaidkamel <osk09876@gmail.com>
2024-02-23 04:59:37 -0800

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
 # Install Flask version 2.1.0 using pip3
 
 exec { 'Flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.5.0',
-  ensure => '2.1.0',
-  provider => 'pip3',
-  require => Package['python3-pip'],
+  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
 }
